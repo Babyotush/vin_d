0245f5cf216c98d2ae385285c916400edf8626c4
Babyotush <95118700+Babyotush@users.noreply.github.com>
2024-07-06 10:43:46 +0100

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -276,6 +276,3 @@ def unpack_new_file_id(new_file_id):
     )
     file_ref = encode_file_ref(decoded.file_reference)
     return file_id, file_ref
-
-# Ensure indexes during initialization
-await ensure_indexes()
